63353d2fdc784fc17c54b70f1eb414e17d7c9c32
Łukasz Sokołowski <264257@student.pwr.edu.pl>
2024-05-28 14:07:39 +0200

    fixed error in tests

--- test_example.py
+++ test_example.py
@@ -1,7 +1,7 @@
 import pytest
 import utils
 
-@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 4, 5), (3, 4, 7), (4, 5, 9)])
+@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7), (4, 5, 9)])
 def test_add(a, b, expected):
     result = utils.add(a, b)
     assert result == expected
